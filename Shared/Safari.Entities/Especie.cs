11313064646196ee04e61a04e48ddd2b61797d86
navegador <navegador@030_01_27_72729>
2019-08-30 21:39:38 -0300

    Arquitectura base

---
+++ Shared/Safari.Entities/Especie.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+
+
+namespace NTier.Entities
+{
+
+    public partial class Especie : IEntity
+    {
+        [DisplayName("Id")]
+        public int Id { get; set; }
+
+
+        [DisplayName("Nombre")]
+        [Required]
+        public string Nombre { get; set; }
+
+        public override string ToString()
+        {
+            return this.GetType().Name + ": " +
+                string.Join(",", this.GetType().GetProperties()
+                .Where(p => !p.PropertyType.IsGenericType && !p.PropertyType.IsArray)
+                .Select(p => string.Format("{0}={1}", p.Name, p.GetValue(this, null))));
+        }
+    }
+}
