af9e84eb6f76bc7f43b67950ef6490fb690f3bc8
navegador <navegador@030_01_27_72729>
2019-09-06 22:07:41 -0300

    Boilerplate

---
+++ Presentation/Safari.UI.Web/Constants/Application.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Safari.UI.Web.Constants
+{
+    public class Application
+    {
+        public const string Name = "ASP.NET MVC Vitruvio";
+        public const string ShortName = "Vitruvio";
+    }
+}
