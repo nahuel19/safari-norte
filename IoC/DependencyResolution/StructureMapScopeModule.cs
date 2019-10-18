e07cd558165b5402e2e84ffb787e384b1d652d7e
navegador <navegador@030_01_28_72736>
2019-10-18 20:51:48 -0300

    DI Cambios

---
+++ IoC/DependencyResolution/StructureMapScopeModule.cs
@@ -0,0 +1,25 @@
+namespace IoC.DependencyResolution {
+    using System.Web;
+
+    //using IoC.App_Start;
+
+    using StructureMap.Web.Pipeline;
+    using App_Start;
+
+    public class StructureMapScopeModule : IHttpModule {
+        #region Public Methods and Operators
+
+        public void Dispose() {
+        }
+
+        public void Init(HttpApplication context) {
+            context.BeginRequest += (sender, e) => StructuremapMvc.StructureMapDependencyScope.CreateNestedContainer();
+            context.EndRequest += (sender, e) => {
+                HttpContextLifecycle.DisposeAndClearAll();
+                StructuremapMvc.StructureMapDependencyScope.DisposeNestedContainer();
+            };
+        }
+
+        #endregion
+    }
+}
